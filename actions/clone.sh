0dd909527b3c042d9aae41728f4e84f9900f27c3
Sean <a1668979@student.adelaide.edu.au>
2017-11-20 20:16:45 +0000

    Making some cool little changes

--- actions/clone.sh
+++ actions/clone.sh
@@ -1,5 +1,19 @@
 #! /bin/sh
 
 _clone_meta() {
+  if [ ${1:-''} = '-h' ];then
+  >&2 cat << EOF
+`basename $0` clone -- information
+
+This action will clone a meta repo and all of its submodules with it.
+
+Usage:
+> `basename $0` clone [-h] <gitpath> [directory]
+
+This work the exact same as git clone but provides the ability to fetch
+submodules at the same time.
+EOF
+  exit 0
+  fi
   git clone --recursive $@
 }
--- actions/view.sh
+++ actions/view.sh
@@ -1,6 +1,22 @@
 #! /bin/sh
 
 _view_submodules() {
+  if [ ${1:-''} = '-h' ];then
+    >&2 cat << EOF
+`basename $0` view -- Information
+
+This action will show the current state of a meta repo's submodules.
+
+Usage:
+> `basename $0` view [-h] [path]
+h)
+  Prints the help message
+path
+  The local file path to the meta repo
+
+EOF
+  exit 0
+  fi
   # Some really terrible printfs
   # Don't read this code.... It will be bad for your health
   printf "%-30s\t%-10s\t%-10s\t%-10s\n" "Project" "Short Hash" "Branch" "Uncommited changes?"
