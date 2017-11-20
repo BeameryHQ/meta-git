368e7d6c982f5c7072798734424b575118c5fe4b
Sean <a1668979@student.adelaide.edu.au>
2017-11-20 19:42:49 +0000

    Making things more git like

--- actions/pull.sh
+++ actions/pull.sh
@@ -1,14 +1,13 @@
 #! /bin/sh
 
-_fetch_changes() {
+_pull_changes() {
     if [ ${1:-''} = "-h" ];then
       >&2 cat << EOF
-`basename $0` fetch -- Information
+`basename $0` pull -- Information
 
 Usage:
-> `basename $0` fetch [-h]
-This will fetch and apply all remote changes to the projects
-and apply them locally.
+> `basename $0` pull [-h]
+This will pull all remote changes to the projects and apply them locally.
 
 Note:
 The default branch used to fetch from is master, if you need to change this
@@ -18,5 +17,5 @@ EOF
     fi
   # This will get all the updates from remote and checkout those changes
   git submodule init
-  git submodule update --recursive --remote --init --checkout $@
+  git submodule update --recursive --remote $@
 }
