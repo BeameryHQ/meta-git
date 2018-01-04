b4e9985e0968a2d7ef996a86897ce33917559bf3
Sean <a1668979@student.adelaide.edu.au>
2018-01-04 11:13:57 +0000

    Successfully fixed this issues with intalling on OSX

--- core/install.sh
+++ core/install.sh
@@ -1,5 +1,4 @@
 #! /bin/sh
-TMPD=`mktemp -d`
 _info_install() {
   cat << EOF
 Installs a driver for $(basename "${0}") on the local system for global shell use.
@@ -13,19 +12,25 @@ $(basename "${0}") install -- Information
 
 This will install $(basename "${0}") on your local machine to
 make it accessible globally.
+Note:
+-   This needs to be run as root.
 EOT
     exit 0
+  fi
+  if [ "$(whoami)" != "root" ];then
+    >&2 echo "[ERROR] Installer requires root privilages"
+    exit -1
   fi
   # Being sneaky, like the ninja, as to how we 'install' this
   # As (AFAIK) bash and zsh do not like to exec softlink as part
   # of the shell, we are going to handle that indirection for the shell;
   # this is so we can update this script without having the user update
   # each time.
-
+  TMPD="$(mktemp -d)"
   DRIVER_DIRECTORY="$(cd "$(dirname "$0")" && pwd -P || exit -1)"
-  DRIVER_LOCATION=$(echo "${DRIVER_DIRECTORY}/$(basename "$0")")
+  DRIVER_LOCATION="${DRIVER_DIRECTORY}/$(basename "$0")"
 
-  cat << EOF > $TMPD/"$(basename "${0}")"
+  cat << EOF > "$TMPD/$(basename "${0}")"
 #! /bin/sh
 
 # ==================== NOTE ====================
@@ -68,11 +73,8 @@ fi
 
 "${DRIVER_LOCATION}" "\$@"
 EOF
-  touch $TMPD/"$(basename "${0}")"
-  chmod a+x $TMPD/"$(basename "${0}")"
-  if [ $? -ne 0 ]; then 
-    echo "[INFO] Need permission to install the application"
-    exit 1
-  fi
-  sudo mv $TMPD/"$(basename "${0}")" /usr/local/bin/"$(basename "${0}")"
+  chmod a+x "${TMPD}/$(basename "${0}")"
+  mv "${TMPD}/$(basename "${0}")" /usr/local/bin/"$(basename "${0}")"
+  rmdir "${TMPD}"
+  >&2 echo "[INFO] successfully installed"
 }
